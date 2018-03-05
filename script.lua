49290c162c28b6711f5a8f333261fbb808c47e81
root <root@also.alice>
2018-03-05 02:11:02 +0000

    Some housekeeping

--- script.lua
+++ script.lua
@@ -1,3 +1,5 @@
+me = 121913323
+
 function vardump(value, depth, key)
   local linePrefix = ""
   local spaces = ""
@@ -38,8 +40,8 @@ end
 
 local lgi = require ('lgi')
 local notify = lgi.require('Notify')
+local utf8 = require('utf8')
 notify.init ("Telegram updates")
-local utf8 = require('lua-utf8')
 
 chats = {}
 
@@ -48,37 +50,32 @@ function do_notify (user, msg)
   n:show ()
 end
 
+function strike (s)
+--s = string.gsub(s, "[\192-\255][\128-\191]*", "%1" .. utf8.char(822))
+  s = string.gsub(s, "[\000-\127]","%1" .. utf8.char(822))
+  s = string.gsub(s,"[\192-\255][\128-\191]", "%1" .. utf8.char(822))
+  s = string.gsub(s,"~","")
+  return s
+end
+
+
 function dl_cb (arg, data)
 end
 
-function strike (s)
-          --s = string.gsub(s, "[\192-\255][\128-\191]*", "%1" .. utf8.char(822))
-          s = string.gsub(s, "[\000-\127]","%1" .. utf8.char(822))
-          s = string.gsub(s,"[\192-\255][\128-\191]", "%1" .. utf8.char(822)) 
-          s = string.gsub(s,"~","")
-          return s
-          end
-
 function tdcli_update_callback (data)
-  --  vardump(data)
-
---This is what handles the strikethru editing (plus the function strike above). 
-  if(data.ID == "UpdateNewMessage") then
-    if((data.message_.send_state_.ID == "MessageIsSuccessfullySent") and (data.message_.can_be_edited_ == true) and (data.message_.sender_user_id_ == my_id)) then
-      local editmsg = data.message_
-        _, _, textToStrike = string.find(editmsg.content_.text_,"(%b~~)")
-        if (textToStrike) then textStruckthru = strike(textToStrike) 
-        editmsg.content_.text_ = string.gsub(editmsg.content_.text_,textToStrike,textStruckthru)
-   tdcli_function ({ID="EditMessageText", chat_id_=editmsg.chat_id_, message_id_=editmsg.id_, reply_markup_=nil, input_message_content_={ID="InputMessageText", text_=editmsg.content_.text_, disable_web_page_preview_=1, clear_draft_=0,entities_ = {}, parse_mode_=editmsg.parse_mode_}}, dl_cb, nil)
-   end
- end
- end
-
---This is the forwarder / messagekeeper / notifier
   if (data.ID == "UpdateNewMessage") then
     local msg = data.message_
     local d = data.disable_notification_
     local chat = chats[msg.chat_id_]
+
+    if((data.message_.send_state_.ID == "MessageIsSuccessfullySent") and (data.message_.can_be_edited_ == true) and (data.message_.sender_user_id_ == me)) then
+	local editmsg = data.message_
+        _, _, textToStrike = string.find(editmsg.content_.text_,"(%b~~)")
+        if (textToStrike) then textStruckthru = strike(textToStrike)
+         editmsg.content_.text_ = string.gsub(editmsg.content_.text_,textToStrike,textStruckthru)
+         tdcli_function ({ID="EditMessageText", chat_id_=editmsg.chat_id_, message_id_=editmsg.id_, reply_markup_=nil, input_message_content_={ID="InputMessageText", text_=editmsg.content_.text_, disable_web_page_preview_=1, clear_draft_=0,entities_ = {}, parse_mode_=editmsg.parse_mode_}}, dl_cb, nil)
+        end
+    end
 
     if ((not d) and chat) then
       if msg.content_.ID == "MessageText" then
@@ -87,10 +84,8 @@ function tdcli_update_callback (data)
         do_notify (chat.title_, msg.content_.ID)
       end
     end
-
-
     if msg.content_.ID == "MessageText" then
-       tdcli_function ({ID="SendMessage", chat_id_=my_id, reply_to_message_id_=msg.id_, disable_notification_=0, from_background_=1, reply_markup_=nil, input_message_content_={ID="InputMessageForwarded", from_chat_id_=msg.chat_id_, message_id_=msg.id_, in_game_share_=0 }}, dl_cb, nil)    
+       tdcli_function ({ID="SendMessage", chat_id_=me, reply_to_message_id_=msg.id_, disable_notification_=0, from_background_=1, reply_markup_=nil, input_message_content_={ID="InputMessageForwarded", from_chat_id_=msg.chat_id_, message_id_=msg.id_, in_game_share_=0 }}, dl_cb, nil)    
     end
   elseif (data.ID == "UpdateChat") then
     chat = data.chat_
@@ -99,3 +94,4 @@ function tdcli_update_callback (data)
     tdcli_function ({ID="GetChats", offset_order_="9223372036854775807", offset_chat_id_=0, limit_=20}, dl_cb, nil)    
   end
 end
+
